92c5326e5ee92e8b829852a60865cec41559d27a
ar4uk <ml_serg@mail.ru>
2026-09-05 13:17:14 +0500

    add selectCatalog

---
+++ app/components/SelectField.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="select-wrapper">
+    <select v-model="model" class="select">
+      <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { options } = defineProps<{
+  options: {
+    label: string
+    value: string
+  }[]
+}>()
+const model = defineModel<string>()
+</script>
+
+<style scoped>
+  .select-wrapper {
+    position: relative;
+  }
+
+  .select {
+    appearance: none;
+    background-color: transparent;
+    border: 1px solid var(--color-gray);
+    padding: 15px 12px;
+    margin: 0;
+    width: 100%;
+    font-family: inherit;
+  }
+</style>
--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,11 +1,49 @@
 <template>
-  <div>catalog</div>
+  <div>
+    <h1 class="left">Каталог товаров</h1>
+    <div class="catalog">
+      <div class="catalog__filter">
+        <SelectField v-model="select" :options="categoriesSelect" />
+      </div>
+      <div></div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import type { GetCategoriesResponse } from '~/components/interfaces/category.interfaces'
+import SelectField from '~/components/SelectField.vue'
 
+const config = useRuntimeConfig()
+const API_URL = config.public.apiurl
+
+const selectDefault = {
+  value: '',
+  label: 'Категории'
+}
+
+const select = ref(selectDefault)
+
+const { data } = await useFetch<GetCategoriesResponse>(API_URL + '/categories')
+
+
+
+const categoriesSelect = computed(() => {
+  return data.value
+    ? data.value?.categories.map((c) => ({
+        value: c.id.toString(),
+        label: c.name,
+      })).concat(selectDefault)
+    : [selectDefault]
+})
 </script>
 
 <style scoped>
-
+.catalog {
+  display: flex;
+  gap: 3;
+}
+.catalog__filter {
+  width: 260px;
+}
 </style>
