69cd4b200e95591e61bef9644c17a9d530953135
ar4uk <ml_serg@mail.ru>
2026-09-09 15:49:36 +0500

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   runtimeConfig: {
     // задается в .env
     token: '', // для сервера
@@ -16,7 +22,7 @@ export default defineNuxtConfig({
       title: 'Лабаз',
       titleTemplate: '%s | Шикарный',
       htmlAttrs: {
-        lang: 'ru'
+        lang: 'ru',
       },
       link: [
         {
@@ -35,6 +41,42 @@ export default defineNuxtConfig({
       },
     ],
   },
+  site: {
+    url: 'http://localhost:3001',
+    name: 'Название сайта',
+  },
+
+  routeRules: {
+    // задать приоритет и частоту конкретной странице
+    '/catalog': {
+      sitemap: { changefreq: 'daily', priority: 0.8 },
+      robots: false,
+    },
+
+    // убрать целую ветку из sitemap
+    '/account/**': { sitemap: false },
+
+    // паттерны тоже работают
+    '/blog/**': { sitemap: { changefreq: 'weekly' } },
+  },
+  robots: {
+    disallow: ['/account'],
+  },
+  sitemap: {
+    defaults: {
+      lastmod: new Date().toISOString(),
+      priority: 0.5,
+      changefreq: 'weekly',
+    },
+  },
+  nitro: {
+    storage: {
+      db: {
+        driver: 'fs-lite',
+        base: './db',
+      },
+    },
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
@@ -44,5 +86,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots',
   ],
 })
